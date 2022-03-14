8cf26cd6e81a3dd7a1ee4ce31ac4b46aef9e2e96
ChuThuong <19h1120035@sv.ut.edu.vn>
2022-03-14 21:25:07 +0700

    sdggsd

--- index.php
+++ index.php
@@ -1,18 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php 
+    session_start();
+    if(!isset($_SESSION['unique_id'])) {
+        header('Location: login.php');
+    }
+?>
+<?php 
+    include_once "header.php";
+?>
+
 <body>
-    <form action="">
-        Name:<input type="text" placeholder="Name">
-        <br>
-        Password:<input type="text" Placeholder="Password">
-        <br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="wrapper">
+        <section class="users">
+            <header>
+            <?php 
+                include_once "php/config.php";
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                if(mysqli_num_rows($sql) > 0) {
+                    $row = mysqli_fetch_assoc($sql);
+
+                }
+            ?>    
+                <div class="content">
+                    <img src="php/images/<?php echo $row['img']; ?>" alt="">
+                    <div class="details">
+                        <span><?php echo $row['fname'] .' '.$row['lname']; ?></span>
+                        <p><?php echo $row['status']; ?></p>
+                    </div>
+                </div>
+                <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
+            </header>
+            <div class="search">
+                <span class="text">Select an user to start chat</span>
+                <input type="text" placeholder="Enter name to search...">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+            <div class="users-list">
+                
+            </div>
+        </section>
+    </div>
+    <script src="./javascript/1.js"></script>
 </body>
+
 </html>
